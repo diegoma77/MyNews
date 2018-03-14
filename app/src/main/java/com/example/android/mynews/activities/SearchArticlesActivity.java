13ff8d3bd020d6dd3679a50fd53cb08fb2bcb8d2
diegoma77 <31185512+diegoma77@users.noreply.github.com>
2018-03-14 19:43:12 +0000

    Added if/else statements. Checkboxes not working

--- app/src/main/java/com/example/android/mynews/activities/SearchArticlesActivity.java
+++ app/src/main/java/com/example/android/mynews/activities/SearchArticlesActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.mynews.R;
@@ -29,6 +30,22 @@ public class SearchArticlesActivity extends AppCompatActivity {
     private CheckBox cb_sports;
     private CheckBox cb_travel;
 
+    //Textviews to check if the value of the variables is the correct one according to checkboxes
+    private TextView tv_arts;
+    private TextView tv_business;
+    private TextView tv_entrepeneurs;
+    private TextView tv_politics;
+    private TextView tv_sports;
+    private TextView tv_travel;
+
+    //Variables for storing changes (related to Chechboxes)
+    private boolean arts_checked;
+    private boolean business_checked;
+    private boolean entrepeneurs_checked;
+    private boolean politics_checked;
+    private boolean sports_checked;
+    private boolean travel_checked;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,12 +58,39 @@ public class SearchArticlesActivity extends AppCompatActivity {
         actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        //Checkboxes
         cb_arts = (CheckBox) findViewById(R.id.search_checkBox_arts);
         cb_business = (CheckBox) findViewById(R.id.search_checkBox_business);
         cb_entrepeneurs = (CheckBox) findViewById(R.id.search_checkBox_entrepeneurs);
         cb_politics = (CheckBox) findViewById(R.id.search_checkBox_politics);
         cb_sports = (CheckBox) findViewById(R.id.search_checkBox_sports);
         cb_travel = (CheckBox) findViewById(R.id.search_checkBox_travel);
+
+        //TextViews
+        tv_arts = (TextView) findViewById(R.id.tv_arts);
+        tv_business = (TextView) findViewById(R.id.tv_business);
+        tv_entrepeneurs = (TextView) findViewById(R.id.tv_entrepeneurs);
+        tv_politics = (TextView) findViewById(R.id.tv_politics);
+        tv_sports = (TextView) findViewById(R.id.tv_sports);
+        tv_travel = (TextView) findViewById(R.id.tv_travel);
+
+        if (arts_checked) tv_arts.setText("true");
+        else tv_arts.setText("false");
+
+        if (business_checked) tv_business.setText("true");
+        else tv_business.setText("false");
+
+        if (entrepeneurs_checked) tv_entrepeneurs.setText("true");
+        else tv_entrepeneurs.setText("false");
+
+        if (politics_checked) tv_politics.setText("true");
+        else tv_politics.setText("false");
+
+        if (sports_checked) tv_sports.setText("true");
+        else tv_sports.setText("false");
+
+        if (travel_checked) tv_travel.setText("true");
+        else tv_travel.setText("false");
 
     }
 
@@ -69,41 +113,29 @@ public class SearchArticlesActivity extends AppCompatActivity {
         // Check which checkbox was clicked
         switch(view.getId()) {
             case R.id.search_checkBox_arts:
-                if (checked) { Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show(); }
-                // Put some meat on the sandwich
-            else { Toast.makeText(this, "Not Checked", Toast.LENGTH_SHORT).show(); }
-                // Remove the meat
+                if (checked) { arts_checked = true; }
+                else { arts_checked = false; }
                 break;
             case R.id.search_checkBox_business:
-                if (checked) { Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show(); }
-                // Put some meat on the sandwich
-            else
-                // Remove the meat
+                if (checked) { business_checked = true; }
+                else { business_checked = false; }
                 break;
             case R.id.search_checkBox_entrepeneurs:
-                if (checked) { Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show(); }
-                // Put some meat on the sandwich
-            else
-                // Remove the meat
+                if (checked) { entrepeneurs_checked = true; }
+                else { entrepeneurs_checked =  false; }
                 break;
             case R.id.search_checkBox_politics:
-                if (checked) { Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show(); }
-                // Put some meat on the sandwich
-                else
-                    // Remove the meat
-                    break;
+                if (checked) { politics_checked = true; }
+                else { politics_checked = false; }
+                break;
             case R.id.search_checkBox_sports:
-                if (checked) { Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show(); }
-                // Put some meat on the sandwich
-                else
-                    // Remove the meat
-                    break;
+                if (checked) { sports_checked = true; }
+                else { sports_checked = false; }
+                break;
             case R.id.search_checkBox_travel:
-                if (checked) { Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show(); }
-                // Put some meat on the sandwich
-                else
-                    // Remove the meat
-                    break;
+                if (checked) { travel_checked = true; }
+                else { travel_checked = false; }
+                break;
             // TODO: Veggie sandwich
         }
     }
