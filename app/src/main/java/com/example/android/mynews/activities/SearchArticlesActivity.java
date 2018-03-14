9d5841bbdc2feae6ef1818f22202e08a543ea8e6
diegoma77 <31185512+diegoma77@users.noreply.github.com>
2018-03-14 19:26:03 +0000

    add listeners to Checkbox (SearchArticlesActivity)

--- app/src/main/java/com/example/android/mynews/activities/SearchArticlesActivity.java
+++ app/src/main/java/com/example/android/mynews/activities/SearchArticlesActivity.java
@@ -6,7 +6,10 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 import com.example.android.mynews.R;
 
@@ -16,14 +19,15 @@ import com.example.android.mynews.R;
 
 public class SearchArticlesActivity extends AppCompatActivity {
 
-    //Checkboxes
-    private CheckBox check_arts;
-    private CheckBox check_business;
-    private CheckBox check_entrepeneurs;
-    private CheckBox check_politics;
-    private CheckBox check_sports;
-    private CheckBox check_travel;
+    private Button button_search;
 
+    //Checkboxes
+    private CheckBox cb_arts;
+    private CheckBox cb_business;
+    private CheckBox cb_entrepeneurs;
+    private CheckBox cb_politics;
+    private CheckBox cb_sports;
+    private CheckBox cb_travel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +40,13 @@ public class SearchArticlesActivity extends AppCompatActivity {
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        cb_arts = (CheckBox) findViewById(R.id.search_checkBox_arts);
+        cb_business = (CheckBox) findViewById(R.id.search_checkBox_business);
+        cb_entrepeneurs = (CheckBox) findViewById(R.id.search_checkBox_entrepeneurs);
+        cb_politics = (CheckBox) findViewById(R.id.search_checkBox_politics);
+        cb_sports = (CheckBox) findViewById(R.id.search_checkBox_sports);
+        cb_travel = (CheckBox) findViewById(R.id.search_checkBox_travel);
 
     }
 
@@ -50,10 +61,51 @@ public class SearchArticlesActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void onCheckboxClicked(View view) {
 
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
 
-
-
-
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.search_checkBox_arts:
+                if (checked) { Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show(); }
+                // Put some meat on the sandwich
+            else { Toast.makeText(this, "Not Checked", Toast.LENGTH_SHORT).show(); }
+                // Remove the meat
+                break;
+            case R.id.search_checkBox_business:
+                if (checked) { Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show(); }
+                // Put some meat on the sandwich
+            else
+                // Remove the meat
+                break;
+            case R.id.search_checkBox_entrepeneurs:
+                if (checked) { Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show(); }
+                // Put some meat on the sandwich
+            else
+                // Remove the meat
+                break;
+            case R.id.search_checkBox_politics:
+                if (checked) { Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show(); }
+                // Put some meat on the sandwich
+                else
+                    // Remove the meat
+                    break;
+            case R.id.search_checkBox_sports:
+                if (checked) { Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show(); }
+                // Put some meat on the sandwich
+                else
+                    // Remove the meat
+                    break;
+            case R.id.search_checkBox_travel:
+                if (checked) { Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show(); }
+                // Put some meat on the sandwich
+                else
+                    // Remove the meat
+                    break;
+            // TODO: Veggie sandwich
+        }
+    }
 
 }
